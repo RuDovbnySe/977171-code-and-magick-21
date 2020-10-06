4d5a0be8e2490199afc360c17a96875c243b88eb
RuDovbnySe <dovbnya-1983@gmail.com>
2020-10-06 12:45:47 +0300

    Доработал замечания

--- js/stat.js
+++ js/stat.js
@@ -1,23 +1,19 @@
 'use strict';
 
-var CLOUD_WIDTH = 420;
-var CLOUD_HEIGHT = 270;
-var CLOUD_X = 100;
-var CLOUD_Y = 10;
-var GAP = 10;
-var BAR_GAP = 50;
-var BAR_HEIGHT = 150;
-var BAR_WIDTH = 40;
-var TEXT_WIDTH = 48;
-var TEXT_GAP = 10;
+const CLOUD_WIDTH = 420;
+const CLOUD_HEIGHT = 270;
+const CLOUD_X = 100;
+const CLOUD_Y = 10;
+const GAP = 10;
+const BAR_GAP = 50;
+const BAR_HEIGHT = 150;
+const BAR_WIDTH = 40;
+const TEXT_WIDTH = 48;
+const TEXT_GAP = 10;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-
-  ctx.fillStyle = 'black';
-  ctx.fillText('Ура вы победили!', 140, 30, 210);
-  ctx.fillText('Список результатов:', 140, 46, 210);
 };
 
 var getMaxElement = function (arr) {
@@ -36,49 +32,25 @@ window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
+  ctx.fillStyle = 'black';
+  ctx.fillText('Ура вы победили!', 140, 30, 210);
+  ctx.fillText('Список результатов:', 140, 46, 210);
+
   var maxTime = getMaxElement(times);
 
+
   for (var i = 0; i < players.length; i++) {
+    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
     ctx.fillText(players[i], CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_Y + BAR_GAP + BAR_HEIGHT + TEXT_WIDTH);
     ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i, 76 + BAR_HEIGHT - (BAR_HEIGHT * (times[i] / maxTime)));
-  }
 
-  for (i = 0; i < players.length; i++) {
+    var randomSource = Math.ceil(Math.random() * 100);
 
     if (players[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      ctx.fillStyle = 'hsl(240, ' + i * 25 + '%, 50%)';
+      ctx.fillStyle = 'hsl(240, ' + randomSource + '%, 50%)';
     }
     ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_Y + TEXT_WIDTH + TEXT_GAP * 3 + BAR_HEIGHT, BAR_WIDTH * 1, BAR_HEIGHT * (times[i] / maxTime) * (-1));
   }
 };
-
-
-// Пробовал по аналогии с облаком отдельную функцию
-// var firstStroke = 'Ура вы победили!';
-// var secondStroke = 'Список результатов:';
-//
-// var winnerList = function (ctx) {
-//   ctx.fillText(firstStroke, 0, 10);
-//   ctx.fillText(secondStroke, 0, 58);
-//   ctx.font = '16 px PT Mono';
-//   ctx.textBaseline = 'hanging';
-// };
-//
-// window.renderStatistics = function (ctx) {
-//   winnerList(ctx);
-// };
-
-// Пробовал копировать из лекции
-// var canvas = document.getElementById('canvas');
-//
-// var ctx = canvas.getContext('2d');
-
-
-// ctx.font = '16 px PT Mono';
-// ctx.textBaseline = 'hanging';
-// ctx.color = 'black';
-// ctx.fillText('Ура вы победили!', 0, 10);
-// ctx.fillText('Список результатов:', 0, 48);
-
